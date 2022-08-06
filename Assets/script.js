aedaa13c2d372ef4a60fdc5dc2a958302da0457a
git geek <cout.1.endl@gmail.com>
2022-08-06 13:54:56 -0400

    inital commit

---
+++ Assets/script.js
@@ -0,0 +1,35 @@
+// the ask elements for each of the hours we will add task inside these
+var nineTask = $("#9hr");
+var tenTask = $("#10hr");
+var elevTask = $("#11hr");
+var twelTask = $("#12hr");
+var oneTask = $("#1hr");
+var twoTask = $("#2hr");
+var threeTask = $("#3hr");
+var fourTask = $("#4hr");
+var fiveTask = $("#5hr");
+//the save buttons to their respective hour that will call the local storage save functions
+var nineBtn = $("9Btn");
+var tenBtn = $("10Btn");
+var elevBtn = $("11Btn");
+var twelBtn = $("12Btn");
+var oneBtn = $("1Btn");
+var twoBtn = $("2Btn");
+var threeBtn = $("3Btn");
+var fourBtn = $("4Btn");
+var fiveBtn = $("5Btn");
+//one click call the functions btn
+nineBtn.click(saveHour);
+tenBtn.click(saveHour);
+elevBtn = $click(saveHour);
+twelBtn = $click(saveHour);
+oneBtn = $click(saveHour);
+twoBtn = $click(saveHour);
+threeBtn = $click(saveHour);
+fourBtn = $click(saveHour);
+fiveBtn = $click(saveHour);
+
+let planner = {
+  saveTask() {},
+  addTask() {},
+};
